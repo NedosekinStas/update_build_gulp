ea0534d03c1341d80344802f0ff39ec0cc3a30f1
Stas Nedosekin <nedosekinstanislav@gmail.com>
2020-11-18 04:55:23 +0300

    hot reload add html,script,style

---
+++ app/js/index.js
@@ -0,0 +1,1 @@
+// console.log('Is true hot reload');
--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,49 @@
 import gulp from 'gulp';
 import sass from 'gulp-sass';
+import browser_sync from 'browser-sync';
 
+// Html
+export const html = () => {
+  return gulp.src('app/*.html')
+    .pipe(browser_sync.reload({ stream: true }))
+}
 
-export const style = () => {
+// Styles
+export const styles = () => {
   return gulp.src('app/sass/**/*.sass')
     .pipe(sass())
     .pipe(gulp.dest('app/css'))
+    .pipe(browser_sync.reload({ stream: true }))
 };
+
+// Scripts
+export const scripts = () => {
+  return gulp.src(['app/js/index.js', 'app/libs/**/*.js'])
+    .pipe(browser_sync.reload({ stream: true }))
+}
+
+export const hotreload = () => {
+  browser_sync.init({
+    notify: false,
+    ui: false,
+    server: {
+	  baseDir: 'app'
+	}
+  });
+}
+
+export const watch = () => {
+  gulp.watch('app/sass/**/*.sass', gulp.series(styles));
+  gulp.watch('app/*.html', gulp.series('html'));
+  gulp.watch(['app/js/index.js', 'app/libs/**/*.js'], gulp.series('scripts'));
+}
 
 export default gulp.series(
   gulp.parallel(
-    style
+    styles
+  ),
+  gulp.parallel(
+    watch,
+    hotreload
   )
-)
+);
